4e3a4da77d1d1b8fc000465d1c5248451fb328ab
robvclarke <robvclarke@gmail.com>
2021-10-11 22:43:00 +0000

    Add the rest of the questions and answers to the array

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,4 +11,36 @@ const questions =
         text:'Which carried 138 million passengers in 2019?',
         answer: "bus"
     }
+    {
+        text:'Which was featured in a scene in ‘The Commitments’ with the cast singing ‘Destination Anywhere’?',
+        answer: "dart"
+    }
+    {
+        text:'Which of these means ‘speed’ in Irish?',
+        answer: "luas"
+    }
+    {
+        text:'Which recently had misinformation on twitter claiming it was now free to ride?',
+        answer: "luas"
+    }
+    {
+        text:'On which of these do 90 per cent of passengers always say thank you to their driver?',
+        answer: "bus"
+    }
+    {
+        text:'Which of these services operate a ghost tour?',
+        answer: "bus"
+    }
+    {
+        text:'Which of these are manufactured in La Rochelle, France?',
+        answer: "luas"
+    }
+    {
+        text:'More than 6,000 homes could be powered by the electricity it takes to run which every year?',
+        answer: "dart"
+    }
+    {
+        text:'Former Eurovision presenter Doireann Ní Bhriain is the announcer voice on which of these??',
+        answer: "luas"
+    }
 ] 
