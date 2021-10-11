4bd643b004617d08b3115a86539c5dfd111e5e59
robvclarke <robvclarke@gmail.com>
2021-10-11 22:35:29 +0000

    Add array of objects with questions and answers

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+var points = 0;
+var question = 0;
+
+const questions = 
+[
+    {
+        text:'Which of these Opened in 1984?',
+        answer: "dart"
+    },
+    {
+        text:'Which carried 138 million passengers in 2019?',
+        answer: "bus"
+    }
+] 
